94e54ba225069c6be5c4c75848dc10da4ea89958
ferenc-attila <attila.ferenc.80@gmail.com>
2022-08-24 08:22:30 +0200

    FormattedPayment PrimeChecker SubstringComparison

---
+++ hu/ferencattila/hackerrank/src/main/java/prepare/java/introduction/FormattedPayment.java
@@ -0,0 +1,26 @@
+package prepare.java.introduction;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FormattedPayment {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        Locale localeOfIndia = new Locale("hi", "IN");
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(localeOfIndia).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
