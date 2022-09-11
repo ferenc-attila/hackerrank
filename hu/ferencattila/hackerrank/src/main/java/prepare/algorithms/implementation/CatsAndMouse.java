ff13fb3eddd0ebe839fbe687142456ba52b89b3b
ferenc-attila <attila.ferenc.80@gmail.com>
2022-09-11 16:41:34 +0200

    Algorithms CatsAndMouse solution

---
+++ hu/ferencattila/hackerrank/src/main/java/prepare/algorithms/implementation/CatsAndMouse.java
@@ -0,0 +1,20 @@
+package prepare.algorithms.implementation;
+
+public class CatsAndMouse {
+
+    static String catAndMouse(int x, int y, int z) {
+        int value = Math.abs(x - z) - Math.abs(y - z);
+        if (value < 0) {
+            return "Cat A";
+        }
+        if (value > 0) {
+            return "Cat B";
+        }
+        return "Mouse C";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(catAndMouse(1,2,3));
+        System.out.println(catAndMouse(1,3,2));
+    }
+}
