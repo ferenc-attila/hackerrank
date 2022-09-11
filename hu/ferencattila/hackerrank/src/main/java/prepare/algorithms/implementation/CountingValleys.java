a9e16ffb94c35122e0c05d0213a59898321d75d9
ferenc-attila <attila.ferenc.80@gmail.com>
2022-09-11 16:08:21 +0200

    Algorithms CountingValleys solution

---
+++ hu/ferencattila/hackerrank/src/main/java/prepare/algorithms/implementation/CountingValleys.java
@@ -0,0 +1,25 @@
+package prepare.algorithms.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountingValleys {
+
+    public static int countingValleys(int steps, String path) {
+        int actualHeight = 0;
+        int numberOfValleys = 0;
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == 'U') {
+                actualHeight++;
+            } else {
+                if (actualHeight == 0) {
+                    actualHeight--;
+                    numberOfValleys++;
+                } else {
+                    actualHeight--;
+                }
+            }
+        }
+        return numberOfValleys;
+    }
+}
